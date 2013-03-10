6786b18857116784d3a92145cde74f541d5f308f
Jonas Bernoulli <jonas@bernoul.li>
2013-03-10 22:29:09 +0100

    initial import

---
+++ hl-todo.el
@@ -0,0 +1,94 @@
+;; * hl-todo.el --- highlight TODO keywords in comments
+;; ** :
+
+;; Copyright (C) 2013  Jonas Bernoulli
+
+;; Author: Jonas Bernoulli <jonas@bernoul.li>
+;; Created: 20130310
+;; Version: 1.0.0
+;; Homepage: http://github.com/tarsius/hl-todo
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;; ** License:
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;; ** Commentary:
+
+;; Hightlight TODO keywords in comments.  There are many minor modes
+;; like it but this one is mine.  It also happens to be simpler than
+;; the alternatives.  For now; I might extend it.
+
+;; * Code:
+
+(require 'org-faces)
+
+(defgroup hl-todo nil
+  "Highlight TODO keywords in comments."
+  :group 'convenience)
+
+(defface hl-todo
+  '((t (:bold t :foreground "#cc9393")))
+  "Face used to highlight TODO keywords."
+  :group 'hl-todo)
+
+(defvar hl-todo-keywords nil)
+
+(defcustom hl-todo-keyword-faces
+  '(("HOLD" . "#d0bf8f")
+    ("TODO" . "#cc9393")
+    ("NEXT" . "#dca3a3")
+    ("THEM" . "#dc8cc3")
+    ("PROG" . "#7cb8bb")
+    ("OKAY" . "#7cb8bb")
+    ("DONT" . "#5f7f5f")
+    ("FAIL" . "#8c5353")
+    ("DONE" . "#afd8af")
+    ("FIXME" . "#cc9393"))
+  "Faces used to highlight specific TODO keywords."
+  :group 'hl-todo
+  :type '(repeat (cons (string :tag "Keyword")
+                       (choice :tag "Face   "
+                               (string :tag "Color")
+                               (sexp :tag "Face"))))
+  :set (lambda (symbol value)
+         (set-default symbol value)
+         (setq hl-todo-keywords
+               `((,(concat "^[\s\t]*;+.*?\\_<\\("
+                           (mapconcat 'car value "\\|")
+                           "\\)\\_>")
+                  (1 (hl-todo-get-face) t))))))
+
+(defun hl-todo-get-face ()
+  (let ((f (cdr (assoc (match-string 1) hl-todo-keyword-faces))))
+    (if (stringp f) (list :inherit 'hl-todo :foreground f) f)))
+
+(define-minor-mode hl-todo-mode
+  "Highlight TODO tags in comments."
+  :lighter ""
+  :group 'hl-todo
+  (if hl-todo-mode
+      (font-lock-add-keywords  nil hl-todo-keywords 'append)
+    (font-lock-remove-keywords nil hl-todo-keywords))
+  (font-lock-fontify-buffer))
+
+;; * :
+(provide 'hl-todo)
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; eval: (when (featurep 'outshine) (outline-minor-mode 1))
+;; End:
+
