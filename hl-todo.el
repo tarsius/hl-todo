c884c727cec22305eb7166c97c1061c2bd2399a2
Jonas Bernoulli <jonas@bernoul.li>
2013-05-04 03:02:59 +0200

    mention orglink in commentary

--- hl-todo.el
+++ hl-todo.el
@@ -31,14 +31,15 @@
 
 ;; For now at least -- I might extend it.  Or I might abandon it in
 ;; favor of one of the following -- so you might be better of going
-;; straight for one of these.
+;; straight for one of these:
 
 ;; - [[http://emacswiki.org/fic-ext-mode.el][fic-ext-mode]]
 ;; - [[https://github.com/lewang/fic-mode][fic-mode]]
 ;; - [[http://emacswiki.org/FixmeMode][fixme-mode]]
 ;; - [[https://github.com/rolandwalker/fixmee][fixmee]]
+;; - see http://emacswiki.org/FixmeMode for more alternatives
 
-;; Also see http://emacswiki.org/FixmeMode.
+;; If you like this you might also like [[https://github.com/tarsius/orglink][orglink]].
 
 ;;; Code:
 
