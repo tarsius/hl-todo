8eb101d65467a670a90a1bd289830ef9bee4cc95
Jonas Bernoulli <jonas@bernoul.li>
2015-03-07 13:41:20 +0100

    make commentary less scary

--- hl-todo.el
+++ hl-todo.el
@@ -26,19 +26,17 @@
 
 ;; Hightlight TODO keywords.  There are many minor modes like it
 ;; but this one is mine.  It also happens to be simpler than the
-;; alternatives.
-
-;; For now at least -- I might extend it.  Or I might abandon it
-;; in favor of one of the following -- so you might be better of
-;; going straight for one of these:
+;; following alternatives:
 
 ;; - [[http://emacswiki.org/fic-ext-mode.el][fic-ext-mode]]
 ;; - [[https://github.com/lewang/fic-mode][fic-mode]]
 ;; - [[http://emacswiki.org/FixmeMode][fixme-mode]]
 ;; - [[https://github.com/rolandwalker/fixmee][fixmee]]
-;; - see http://emacswiki.org/FixmeMode for more alternatives
 
-;; If you like this you might also like [[https://github.com/tarsius/orglink][orglink]].
+;; See [[http://emacswiki.org/FixmeMode][this list]] on the Emacswiki for even more alternatives.
+
+;; If you like this you might also like [[https://github.com/tarsius/orglink][orglink]], if you used
+;; this, then the above links would not look so ugly.
 
 ;;; Code:
 
