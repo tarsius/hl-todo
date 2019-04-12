tarsius/hl-todo

commit f7d97056d17ef7fc04f4fa0b65a703d1d1a5a6b8

Highlight ??? again
This wasn't highlighted anymore because "\<?" and "?\>" did not
match "???".  The syntax class of "?" was punctuation, which is
quite reasonable.  Change the syntax in `hl-todo--syntax-table'
to word.  In the regexp replace "\\>" with "\\(?:\\>\\|\\>?\\)"
because otherwise e.g. "TODO?" would no longer match.

Fixes #35.

hl-todo.el
@@ -129,7 +129,7 @@ including alphanumeric characters, cannot be used here."
   (setq hl-todo--regexp
         (concat "\\(\\<"
                 (regexp-opt (mapcar #'car hl-todo-keyword-faces) t)
-                "\\>"
+                "\\(?:\\>\\|\\>?\\)"
                 (and (not (equal hl-todo-highlight-punctuation ""))
                      (concat "[" hl-todo-highlight-punctuation "]*"))
                 "\\)"))
@@ -138,7 +138,10 @@ including alphanumeric characters, cannot be used here."
            (1 (hl-todo--get-face) t t))))
   (font-lock-add-keywords nil hl-todo--keywords t))
 
-(defvar hl-todo--syntax-table (copy-syntax-table text-mode-syntax-table))
+(defvar hl-todo--syntax-table
+  (let ((table (copy-syntax-table text-mode-syntax-table)))
+    (modify-syntax-entry ?? "w" table)
+    table))
 
 (defun hl-todo--search (&optional regexp bound backward)
   (unless regexp

commit 716ada1a8e3f62fb943b777d4bf5f94e3c613e84

Fix end-of-word regexp anchoring
This fixes a regression introduced in
f7d97056d17ef7fc04f4fa0b65a703d1d1a5a6b8, which caused, e.g.,
`TODOxyz` to be partially highlighted.

hl-todo.el
@@ -148,7 +148,7 @@ including alphanumeric characters, cannot be used here."
   (setq hl-todo--regexp
         (concat "\\(\\<"
                 "\\(" (mapconcat #'car hl-todo-keyword-faces "\\|") "\\)"
-                "\\(?:\\>\\|\\>?\\)"
+                "\\(?:\\>\\|\\>\\?\\)"
                 (and (not (equal hl-todo-highlight-punctuation ""))
                      (concat "[" hl-todo-highlight-punctuation "]*"))
                 "\\)"))