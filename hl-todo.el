57378bd4511887a815725a7850e1ff2c6e9fda16
Jonas Bernoulli <jonas@bernoul.li>
2021-05-04 16:06:17 +0200

    Add SPDX-License-Identifier to the library header
    Also shuffle the header a bit.

--- hl-todo.el
+++ hl-todo.el
@@ -8,20 +8,22 @@
 
 ;; Package-Requires: ((emacs "25"))
 
-;; This file is not part of GNU Emacs.
+;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
-
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; For a full copy of the GNU General Public License
 ;; see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of GNU Emacs.
 
 ;;; Commentary:
 
