5054954eed295d6a55c67a6d63babdcefb779869
Jonas Bernoulli <jonas@bernoul.li>
2023-08-15 16:15:06 +0200

    Bump compat dependency

--- hl-todo.el
+++ hl-todo.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/hl-todo
 ;; Keywords: convenience
 
-;; Package-Requires: ((emacs "25.1") (compat "29.1.3.4"))
+;; Package-Requires: ((emacs "25.1") (compat "29.1.4.2"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
