e4588eacf79060391ab42b50410c33bb42739a0b
Jonas Bernoulli <jonas@bernoul.li>
2024-03-29 16:01:52 +0100

    hl-todo-insert: Insert punctuation if appropriate and unambiguous
    Closes #81.

--- hl-todo.el
+++ hl-todo.el
@@ -428,12 +428,17 @@ enabling `flymake-mode'."
 
 ;;;###autoload
 (defun hl-todo-insert (keyword)
-  "Insert TODO or similar keyword.
+  "Read a TODO or similar keyword and insert it at point.
+
 If point is not inside a string or comment, then insert a new
 comment.  If point is at the end of the line, then insert the
 comment there, otherwise insert it as a new line before the
-current line.  When called interactively the KEYWORD is read via
-`completing-read'."
+current line.  When called interactively the KEYWORD is read
+via `completing-read'.
+
+If `hl-todo-require-punctuation' is non-nil and
+`hl-todo-highlight-punctuation' contains a single character,
+then append that character to the inserted string."
   (interactive
    (list (completing-read
           "Insert keyword: "
@@ -442,26 +447,30 @@ current line.  When called interactively the KEYWORD is read via
                          (list (propertize keyword 'face
                                            (hl-todo--combine-face face)))))
                   hl-todo-keyword-faces))))
-  (cond
-   ((hl-todo--inside-comment-or-string-p)
-    (insert (concat (and (not (memq (char-before) '(?\s ?\t))) " ")
-                    keyword
-                    (and (not (memq (char-after) '(?\s ?\t ?\n))) " "))))
-   ((and (eolp)
-         (not (looking-back "^[\s\t]*" (line-beginning-position) t)))
-    (insert (concat (and (not (memq (char-before) '(?\s ?\t))) " ")
-                    (format "%s %s " comment-start keyword))))
-   (t
-    (goto-char (line-beginning-position))
-    (insert (cond ((derived-mode-p 'lisp-mode 'emacs-lisp-mode)
-                   (format "%s%s %s" comment-start comment-start keyword))
-                  ((string-suffix-p " " comment-start)
-                   (format "%s%s" comment-start keyword))
-                  (t
-                   (format "%s %s" comment-start keyword))))
-    (unless (looking-at "[\s\t]*$")
-      (save-excursion (insert "\n")))
-    (indent-region (line-beginning-position) (line-end-position)))))
+  (let ((keyword (if (and hl-todo-require-punctuation
+                          (length= hl-todo-highlight-punctuation 1))
+                     (concat keyword hl-todo-highlight-punctuation)
+                   keyword)))
+    (cond
+     ((hl-todo--inside-comment-or-string-p)
+      (insert (concat (and (not (memq (char-before) '(?\s ?\t))) " ")
+                      keyword
+                      (and (not (memq (char-after) '(?\s ?\t ?\n))) " "))))
+     ((and (eolp)
+           (not (looking-back "^[\s\t]*" (line-beginning-position) t)))
+      (insert (concat (and (not (memq (char-before) '(?\s ?\t))) " ")
+                      (format "%s %s " comment-start keyword))))
+     (t
+      (goto-char (line-beginning-position))
+      (insert (cond ((derived-mode-p 'lisp-mode 'emacs-lisp-mode)
+                     (format "%s%s %s" comment-start comment-start keyword))
+                    ((string-suffix-p " " comment-start)
+                     (format "%s%s" comment-start keyword))
+                    (t
+                     (format "%s %s" comment-start keyword))))
+      (unless (looking-at "[\s\t]*$")
+        (save-excursion (insert "\n")))
+      (indent-region (line-beginning-position) (line-end-position))))))
 
 (define-obsolete-function-alias 'hl-todo-insert-keyword
   #'hl-todo-insert "hl-todo 3.0.0")
