fb692ec0923bcd98eaa8760127921aca61db8e1e
Jonas Bernoulli <jonas@bernoul.li>
2025-01-05 20:56:31 +0100

    Release version 3.8.2

--- hl-todo.el
+++ hl-todo.el
@@ -6,8 +6,8 @@
 ;; Homepage: https://github.com/tarsius/hl-todo
 ;; Keywords: convenience
 
-;; Package-Version: 3.8.1
-;; Package-Requires: ((emacs "26.1") (compat "30.0.0.0"))
+;; Package-Version: 3.8.2
+;; Package-Requires: ((emacs "26.1") (compat "30.0.2.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
